e4e94222b9c64be3d93c8c6c761456f2e7d8af13
Adnan Moghal <adnan.moghal1505@gmail.com>
2024-03-04 10:37:09 +0000

    First ever commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Weather from './Weather';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Weather forecast app</h1>
+      <Weather/>
+    </div>
+  )
+}
+export default App;
