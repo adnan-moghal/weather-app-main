766f876f2eb805a237e49a4bdde83f458e60c6c1
Adnan Moghal <ec22692@qmul.ac.uk>
2024-03-09 21:42:37 +0000

    testing conditionals for expanding button

--- src/MainInfo.js
+++ src/MainInfo.js
@@ -1,5 +1,14 @@
+import { useEffect, useState } from "react"
+
 const MainInfo = ({info}) => {
-    const weatherInfo = info
+    const weatherInfo = info;
+
+    const [clicked, setClicked] = useState(false);
+    
+    const click = (bool) => {
+        setClicked(!bool)
+        console.log(bool)
+    }
 
     if (weatherInfo === null) {
         return (
@@ -18,6 +27,12 @@ const MainInfo = ({info}) => {
                         <p>Humidity : {weatherInfo.main.humidity}%</p>
                         <p>Pressure : {weatherInfo.main.pressure}</p>
                         <p>Wind Speed : {weatherInfo.wind.speed}m/s</p>
+                        <button onClick={() => {click(clicked)}}>test clickers</button>
+                        {clicked ? (
+                            <p>clicked is true</p>
+                        ): (
+                            <p>clicked is flse</p>
+                        )}
                     </>
                 ) : (
                         <p>Please enter a City</p>
